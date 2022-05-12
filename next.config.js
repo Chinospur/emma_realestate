b0bbee5e8bbd8024f246bb6c9b6e4a337361be9b
chino_spur <you@example.com>
2022-05-12 14:24:48 +0100

    first deploy

--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["bayut-production.s3.eu-central-1.amazonaws.com"]
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   }
 }
